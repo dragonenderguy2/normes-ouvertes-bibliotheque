cb83573eb45db9a717830f5c7f1181617a907bb3
dragonenderguy2 <shaihalapre@hotmail.com>
2025-09-05 13:20:22 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+class NormesOuvertes {
+    static utiliserNorme(nomNorme) {
+        // Logique pour utiliser la norme
+        return `Données extraites pour la norme: ${nomNorme}`;
+    }
+}
+
+module.exports = NormesOuvertes;
